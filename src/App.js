4b2658b820f7802dd957386db496c267fcee9fa7
shnooki <vikisahputra17@gmail.com>
2023-05-30 19:47:12 +0700

    done fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://54.169.180.142/');
+      const responseData = await response.json();
+      console.log(responseData)
+
+      setData(responseData);
+    } catch (error) {
+      console.error('Terjadi kesalahan:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data Buku</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.buku}</li>
+        ))}
+      </ul>
     </div>
   );
 }
